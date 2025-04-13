91d9aa00e207214bf53ca7b198b5ac07f78be3d9
unknown <danielzadva@gmail.com>
2025-04-13 23:34:49 +0100

    initial commit

---
+++ variables.js
@@ -0,0 +1,10 @@
+//variable is a container that stores a value
+// it behaves as if it were value it saves
+
+//To create variables there are two steps
+// Declaration and 
+// Assignment
+let x;
+x = 45;
+
+console.log(x);
