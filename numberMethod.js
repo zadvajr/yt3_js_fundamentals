deb2480fc34865e19ead9ae3a053f8f548d7ffb8
unknown <danielzadva@gmail.com>
2025-04-20 17:52:00 +0100

    Number() function

---
+++ numberMethod.js
@@ -0,0 +1,8 @@
+//The Number() function in javascripts convert values to number equivalent
+// if you pass false, null and true it will returns 0, and 1
+
+age = "34";
+console.log(Number(age));
+console.log(false + 1);
+console.log(null + 0);
+console.log(true + 1);
